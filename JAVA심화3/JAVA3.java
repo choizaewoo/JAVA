3d4a83146b23c04cfdb7d8f69a82002b9b566f45
choizaewoo <1994cjw@gmail.com>
2022-05-19 18:34:35 +0900

    자바 심화

---
+++ JAVA심화3/JAVA3.java
@@ -0,0 +1,10 @@
+package JAVA3;
+
+public class JAVA3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ JAVA심화3/MethodTest.java
@@ -0,0 +1,17 @@
+package JAVA3;
+
+public class MethodTest {
+
+	public static void iamMethod() {
+		System.out.println("메소드라 불러줘요~\n"); //메소드 함수를 만들어준다
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MethodTest.iamMethod(); //클래스 안에 함수를 정의하면 함수이름만으로 불러 올 수 있다.
+		
+		iamMethod();
+	}
+
+}
